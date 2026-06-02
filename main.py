f9c11023b304c3e80e227edaf231b81e43f35be3
gustavo-schulz <contaprogithub@outlook.com>
2026-06-02 10:54:40 -0300

    troquei por emoji e formatei

--- main.py
+++ main.py
@@ -5,32 +5,36 @@ def criar_tabuleiro():
     matriz = []
     for i in range(10):
         linha = []
-
         for j in range(10):
-            linha.append("-")
-
+            
+            linha.append("🌊")
         matriz.append(linha)
     return matriz
 
 def mostrar_tabuleiro(tabuleiro, titulo):
-    letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    letras = [" A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
-    print("=== {} ===".format(titulo))
-    print("     {}".format(" ".join(letras)))
+    print("\n=== {} ===".format(titulo))
+    
+  
+    print("     {}".format("  ".join(letras)))
 
     for i in range(len(tabuleiro)):
         print("{:2} | {}".format(i + 1, " ".join(tabuleiro[i])))
 
 def posicionando_navios_jogador(tabuleiro):
-
     letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
     for i in range(5):
-
         while True:
             print("\nPosicionando navio {}".format(i + 1))
 
-            linha = int(input("Linha (1-10): ")) - 1
+            try:
+                linha = int(input("Linha (1-10): ")) - 1
+            except ValueError:
+                print("Linha inválida! Digite apenas números.")
+                continue
+                
             coluna = input("Coluna (A-J): ").upper()
 
             if linha < 0 or linha > 9:
@@ -43,21 +47,22 @@ def posicionando_navios_jogador(tabuleiro):
 
             coluna = letras.index(coluna)
 
-            if tabuleiro[linha][coluna] == "N":
+            
+            if tabuleiro[linha][coluna] == "🛥️ ":
                 print("Já existe um navio nessa posição!")
                 continue
 
-            tabuleiro[linha][coluna] = "N"
+            
+            tabuleiro[linha][coluna] = "🛥️ "
+            
             mostrar_tabuleiro(tabuleiro, "TABULEIRO DO JOGADOR")
             break
 
 def main():
     tabuleiro = criar_tabuleiro()
-
     mostrar_tabuleiro(tabuleiro, "TABULEIRO DO JOGADOR")
-
     posicionando_navios_jogador(tabuleiro)
 
-    mostrar_tabuleiro(tabuleiro, "TABULEIRO DO JOGADOR")
-
-main()
+# Executa o programa
+if __name__ == "__main__":
+    main()
